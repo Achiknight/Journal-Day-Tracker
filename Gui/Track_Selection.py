8ae98e0a80dd709a4997df87ac07526afebfa37d
Achiknight <achirvshah2008@gmail.com>
2026-09-02 21:59:50 +0530

    removed un-necesarry dicts from Track_Selection.py

--- Gui/Track_Selection.py
+++ Gui/Track_Selection.py
@@ -4,13 +4,13 @@ from DBFile import *
 from Backend.selection import selection,selected
 
 buttsopen = {}
-def plece(butt):                #Frame Open / Close Logic
-    if buttsopen[butt] == 1:
-        buttsopen[butt] = 0
-        butt.pack_forget()
-    elif buttsopen[butt] == 0:
-        butt.pack(anchor='n')
-        buttsopen[butt] = 1
+def plece(frm):                #Frame Open / Close Logic
+    if buttsopen[frm] == 1:
+        buttsopen[frm] = 0
+        frm.pack_forget()
+    elif buttsopen[frm] == 0:
+        frm.pack(anchor='n',side="top")
+        buttsopen[frm] = 1
 
 
 Track_Sel = CTkScrollableFrame(root,fg_color=fg)
@@ -18,9 +18,6 @@ Track_Sel = CTkScrollableFrame(root,fg_color=fg)
 Row = CTkFrame(Track_Sel,fg_color=fg)
 Row.pack(anchor='w')
 
-catbutt = {}        #* Category Button
-Dataframe = {}      #* Stores individual Checkbox inside types
-catframe = {}       #* Stores the Frames for checkboxes
 
 col = 3
 for index,category in enumerate(journal_types.keys()):
@@ -30,21 +27,23 @@ for index,category in enumerate(journal_types.keys()):
         Row = CTkFrame(Track_Sel,fg_color=fg)
         Row.pack(anchor='w',pady=40)
         
+    storage = CTkFrame(Row,fg_color=fg)                     #* Storage so that button and slection stack
+    storage.pack(anchor="nw",pady=40,padx=20,side="left")
     
-    catframe[category] = CTkFrame(Row,fg_color=fg)                      #* Adds a frame to  "Frame storage for checkboxes"
+    catframe = CTkFrame(storage,fg_color=fg)                      #* Adds a frame for checkboxes"
     
-    catbutt[category] = Button(Row)                                         #* Adds a button to Category buttons
-    catbutt[category].configure(text=category,command=lambda i=catframe[category]:plece(i))      
-    catbutt[category].pack(anchor="w",side="left",padx=40,pady=20)
-    buttsopen[catframe[category]] = 0                                   #* Open / close logic
+    catbutt = Button(storage)                                         #* Adds a button to Category buttons
+    catbutt.configure(text=category,command=lambda i=catframe:plece(i))      
+    catbutt.pack(anchor="w",side="top",padx=40,pady=20)
+    buttsopen[catframe] = 0                                   #* Open / close logic
 
 
-    Dataframe[category] = {}            #! Creates a Dict for current category
+
     for types in journal_types[category]:               #* to put selection button inside the catframe               
-        Dataframe[category][types] = CTkCheckBox(catframe[category],text=types,fg_color=fg,hover_color=hv,font=(Ft2,20))
-        Dataframe[category][types].pack(anchor="w")
+        Dataframe = CTkCheckBox(catframe,text=types,fg_color=fg,text_color=txt,
+                                                 hover_color=hv,font=(Ft2,20),command=lambda tp=types:selection(tp))
+        Dataframe.pack(anchor="w")
         
-
         
 def placethis():
     Track_Sel.pack(fill="both",anchor="nw",expand = True)
