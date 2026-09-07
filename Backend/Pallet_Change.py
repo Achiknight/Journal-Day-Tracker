658915df46950a7948686187d33feff56885adbc
Achiknight <achirvshah2008@gmail.com>
2026-09-07 23:35:12 +0530

    Fixed Color changeing next up color reloading

--- Backend/Pallet_Change.py
+++ Backend/Pallet_Change.py
@@ -1,5 +1,6 @@
 import json
 from Backend.config import color_var
+from Backend.config import fg,txt,Ft1,Ft2,brd,hv
 
 file = open("Config.json", "r")
 data = json.load(file)
@@ -9,7 +10,7 @@ pallet = data["palettes"]
 
 def color_change(chosen):
     pallet["current"] = pallet[chosen]
-
+    
     file = open("Config.json", "w")
     json.dump(data, file, indent=4)
     file.close()
@@ -38,3 +39,4 @@ def reload_widget_colors(widget):
 
     for child in widget.winfo_children():
         reload_widget_colors(child)
+    print("Called")
--- Backend/config.py
+++ Backend/config.py
@@ -1,16 +1,18 @@
 import json
 var_list =  {"background_color": "fg", "hover_color": "hv", "text_color": "txt", "border_color": "brd", "Font_style1": "Ft1", "Font_style2": "Ft2"}
+def color_var():           #! 
+    file = open(r"Config.json","r")
 
-file = open(r"Config.json","r")
+    data = json.load(file)
+    pallet = data["palettes"]
+    default = pallet["current"]
+        
+    file.close()
 
-data = json.load(file)
-pallet = data["palettes"]
-default = pallet["current"]
 
-def color_var():
     for keys in default.keys():
         globals()[var_list[keys]] = default[keys]
-    
-file.close()
+        print(default[keys])
+
 
 color_var()
