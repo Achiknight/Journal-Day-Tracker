faa58c4182814f75f808c27aa48c590c7a1b5e64
Achiknight <achirvshah2008@gmail.com>
2026-09-19 09:26:56 +0530

    added a function to change b/w sql3 and mysql

---
+++ DBFile/Connection.py
@@ -0,0 +1,17 @@
+import sqlite3
+import mysql.connector as msc
+
+def sql3():
+    data = sqlite3.connect("Journal.db")
+    cur = data.cursor()
+    return data,cur
+
+def Mysql(Host,User,Pass,Database):
+    data = msc.connect(
+        localhost=Host,
+        user = User,
+        password = Pass,
+        database = Database
+    )
+    cur = data.cursor()
+    return data,cur
